e7b098d4351c74f3b3ea5f1fd590dbaef72f4e41
yebology <139052533+yebology@users.noreply.github.com>
2025-06-16 17:52:04 +0700

    delete redundancy on test case

--- test/NusaQuest.test.js
+++ test/NusaQuest.test.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 
 describe("NusaQuest", function () {
-  let nusaQuest, deployer, bob;
+  let nusaQuest, deployer, bob, bobInstance;
   const ids = [0, 1, 2, 3, 4];
   const values = [200, 1, 1, 1, 1];
   const prices = [0, 40, 200, 300, 400];
@@ -16,6 +16,7 @@ describe("NusaQuest", function () {
     [deployer, bob] = await ethers.getSigners();
     const NusaQuest = await ethers.getContractFactory("NusaQuest");
     nusaQuest = await NusaQuest.deploy();
+    bobInstance = nusaQuest.connect(bob);
   });
 
   it("should allow minting both fungible and non-fungible tokens", async function () {
@@ -54,14 +55,11 @@ describe("NusaQuest", function () {
   });
 
   it("should allow claiming fungible token once per day for each destination", async function () {
-    const bobInstance = await nusaQuest.connect(bob);
-    const destinationId = destinationIds[0];
-
     await nusaQuest.mint(ids, values, prices, uris);
 
-    await expect(bobInstance.claim(destinationId, proofs))
+    await expect(bobInstance.claim(destinationIds[0], proofs))
       .to.emit(nusaQuest, "Claimed")
-      .withArgs(bob.address, destinationId, anyValue);
+      .withArgs(bob.address, destinationIds[0], anyValue);
 
     const expectedBalance = 20;
     const actualBalance = await nusaQuest.balance(bob.address, fungibleTokenId);
@@ -74,43 +72,31 @@ describe("NusaQuest", function () {
   });
 
   it("should revert when user tries to claim token more than once per day per destination", async function () {
-    const bobInstance = await nusaQuest.connect(bob);
-    const destinationId = destinationIds[0];
-
     await nusaQuest.mint(ids, values, prices, uris);
-    await bobInstance.claim(destinationId, proofs);
+    await bobInstance.claim(destinationIds[1], proofs);
 
-    await expect(bobInstance.claim(destinationId, proofs)).to.be.revertedWith(
+    await expect(
+      bobInstance.claim(destinationIds[1], proofs)
+    ).to.be.revertedWith(
       "You can only claim once per day for each destination."
     );
   });
 
   it("should allow user to swap claimed fungible tokens for an NFT", async function () {
-    const bobInstance = await nusaQuest.connect(bob);
-    const firstDestinationId = destinationIds[0];
-    const secondDestinationId = destinationIds[1];
-    const nftId = ids[1];
-    const amountToSwap = 40;
-
     await nusaQuest.mint(ids, values, prices, uris);
-    await bobInstance.claim(firstDestinationId, proofs);
-    await bobInstance.claim(secondDestinationId, proofs);
+    await bobInstance.claim(destinationIds[0], proofs);
+    await bobInstance.claim(destinationIds[1], proofs);
 
-    await expect(bobInstance.swap(nftId, amountToSwap))
+    await expect(bobInstance.swap(ids[1], 40))
       .to.emit(nusaQuest, "Swapped")
-      .withArgs(bob.address, nftId);
+      .withArgs(bob.address, ids[1]);
   });
 
   it("should revert when swapped token amount does not match NFT price", async function () {
-    const bobInstance = await nusaQuest.connect(bob);
-    const firstDestinationId = destinationIds[0];
-    const nftId = ids[1];
-    const amountToSwap = 50;
-
     await nusaQuest.mint(ids, values, prices, uris);
-    await bobInstance.claim(firstDestinationId, proofs);
+    await bobInstance.claim(destinationIds[0], proofs);
 
-    await expect(bobInstance.swap(nftId, amountToSwap)).to.be.revertedWith(
+    await expect(bobInstance.swap(ids[1], 50)).to.be.revertedWith(
       "Price mismatch: the provided token amount does not match the NFT price."
     );
   });

1aea2b42f9e5ec01f580070fc4902d033ecd34f6
yebology <139052533+yebology@users.noreply.github.com>
2025-06-16 07:34:44 +0700

    migrate to erc1155

--- contracts/NusaQuest.sol
+++ contracts/NusaQuest.sol
@@ -2,24 +2,57 @@
 
 pragma solidity ^0.8.28;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {ERC721Holder} from "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
-import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155Holder} from "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import {ERC1155URIStorage} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NusaQuest is ERC721URIStorage, ERC721Holder, Ownable {
+contract NusaQuest is ERC1155URIStorage, ERC1155Holder, Ownable {
     //
-    mapping(uint256 => string) private proofs;
+    // mapping(uint256 => string) private proofs;
+    mapping(string => mapping(address => uint256)) lastClaims;
 
-    constructor() Ownable(msg.sender) ERC721("NusaQuest", "NQT") {}
-
-    function mintNFT(uint256 _id, string memory _uri) external onlyOwner {
-        _safeMint(address(this), _id);
-        _setTokenURI(_id, _uri);
+    modifier validBatchInputLengths(
+        uint256 _idsLength,
+        uint256 _valuesLength,
+        uint256 _urisLength
+    ) {
+        require(
+            _idsLength == _valuesLength && _valuesLength == _urisLength,
+            "Mismatch between IDs, values, and URIs. Please ensure all inputs have the same length."
+        );
+        _;
     }
 
-    function claimNFT(uint256 _id, string memory _proof) external {
-        proofs[_id] = _proof;
+    constructor() Ownable(msg.sender) ERC1155("") {
+        _setBaseURI("https://gateway.pinata.cloud/ipfs/");
+    }
+
+    function mint(
+        uint256[] memory _ids,
+        uint256[] memory _values,
+        string[] memory _uris
+    )
+        external
+        onlyOwner
+        validBatchInputLengths(_ids.length, _values.length, _uris.length)
+    {
+        _mintBatch(address(this), _ids, _values, "");
+
+        for (uint256 i = 0; i < _ids.length; i++) {
+            _setURI(_ids[i], _uris[i]);
+        }
+    }
+
+    function claim(string memory _destinationId, string[] memory _proofs) external {
+        lastClaims[_destinationId][msg.sender] = block.timestamp;
+        // proofs[_id] = _proof;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC1155, ERC1155Holder) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
     //
 }
